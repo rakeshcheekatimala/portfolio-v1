690e258420c5a1b0f214c658eb83cabae4bd73a2
rakesh.cheekatimala <rakeshcheekatimala@gmail.com>
2020-09-02 21:39:36 +0800

    :hammer: cleanup for the mainpage under pages folder

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Header, Content } from './../components';
+const Page = () => {
+  return (
+    <>
+      <Header />
+      <Content />
+    </>
+  );
+};
+
+export default Page;
